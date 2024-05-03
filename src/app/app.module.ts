05ff3b4bdb04de701c16cbe502528744bd0cfc24
jeyofdev <jgregoire.dev@gmail.com>
2024-05-03 15:07:43 +0200

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TextFieldComponent } from './components/text-field/text-field.component';
+
+@NgModule({
+  declarations: [AppComponent, TextFieldComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
